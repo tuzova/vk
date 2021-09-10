4692fa8343daa925eb8b27d3f03a2206abb3e73b
Irine Tuzova <alexandrinius@outlook.com>
2021-09-10 20:30:54 +0300

    Revision-2 PostManager

--- src/main/java/manager/PostManager.java
+++ src/main/java/manager/PostManager.java
@@ -3,32 +3,76 @@ package manager;
 import domain.Post;
 
 public class PostManager {
-    private Post[] posts;       // массив объектов класса Post
-    private Search search;      // метод позволяет искать записи на стене в соответствии с заданными критериями.
-    private Delete delete;      // метод удаляет запись со стены
+    private Post[] posts;           // массив объектов класса Post
+    private int ownerId;            // идентификатор пользователя или сообщества
+    private char postId;            // идентификатор записи на стене, положительное число
+    private String domain;          // короткий адрес пользователя или сообщества
+    private String query;           // поисковой запрос, максимальная длина 9000
+    private boolean ownersOnly;     // 1 — возвращать только записи от имени владельца стены. Может принимать значения 1 или 0
+    private int count = 20;         // количество записей, которые необходимо вернуть. Положительное число, по умолчанию 20, максимальное значение 100
+    private int offset;             // смещение, необходимо для получения определенного подмножества результатов. Положительное число, по умолчанию 0
 
-    public Post[] getPosts() {
-        return posts;
+    public int getOwnerId() {
+        return ownerId;
     }
 
-    public void setPosts(Post[] posts) {
-        this.posts = posts;
+    public void setOwnerId(int ownerId) {
+        this.ownerId = ownerId;
     }
 
-    public Search getSearch() {
-        return search;
+    public char getPostId() {
+        return postId;
     }
 
-    public void setSearch(Search search) {
-        this.search = search;
+    public void setPostId(char postId) {
+        this.postId = postId;
     }
 
-    public Delete getDelete() {
-        return delete;
+    public String getDomain() {
+        return domain;
     }
 
-    public void setDelete(Delete delete) {
-        this.delete = delete;
+    public void setDomain(String domain) {
+        this.domain = domain;
+    }
+
+    public String getQuery() {
+        return query;
+    }
+
+    public void setQuery(String query) {
+        this.query = query;
+    }
+
+    public boolean isOwnersOnly() {
+        return ownersOnly;
+    }
+
+    public void setOwnersOnly(boolean ownersOnly) {
+        this.ownersOnly = ownersOnly;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public void setCount(int count) {
+        this.count = count;
+    }
+
+    public int getOffset() {
+        return offset;
+    }
+
+    public void setOffset(int offset) {
+        this.offset = offset;
+    }
+
+    public Post[] searchPosts(int ownerId, char postId, String domain, String query, int ownersOnly, int count, int offset) {
+        return null;
+    }
+
+    public void deletePost(int ownerId, char postId) {
     }
 }
 
